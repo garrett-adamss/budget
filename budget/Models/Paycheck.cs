c07e0be7d2fdfd5c075d6efb263f0cd83678e4b3
Garrett Adams <garrettadams27@gmail.com>
2023-03-02 23:50:07 -0700

    paycheck

---
+++ budget/Models/Paycheck.cs
@@ -0,0 +1,16 @@
+namespace budget.Models;
+
+public class Paycheck
+{
+    public int Id { get; set;}
+    public string AccountId { get; set;}    
+    public Account Account { get; set; }
+    public DateTime PaycheckDate { get; set; }
+    public DateTime PayPeriodStartDate { get; set; }
+    public DateTime PayPeriodEndDate { get; set; }
+    public decimal GrossIncome { get; set; }
+    public decimal TaxAmount { get; set; }
+    public decimal NetIncome { get; set; }
+    public decimal Savings { get; set; }
+    public decimal Tithe { get; set; }
+}
